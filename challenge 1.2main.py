6e25cf9410b571019cb7155837e1588def86e524
Adhithiriya <151902117+Adhithiriya@users.noreply.github.com>
2023-11-24 11:47:57 +0530

    Add files via upload

---
+++ challenge 1.2main.py
@@ -0,0 +1,20 @@
+year = 2004
+month = 7
+
+# To get year (integer input) from the user
+
+if (year % 400 == 0) and (year % 100 == 0):
+    print("{0} is a leap year".format(year))
+
+# not divided by 100 means not a century year
+# year divided by 4 is a leap year
+elif (year % 4 ==0) and (year % 100 != 0):
+    print("{0} is a leap year".format(year))
+
+# if not divided by both 400 (century year) and 4 (not century year)
+# year is not leap year
+else:
+    print("{0} is not a leap year".format(year))
+
+24
+21
